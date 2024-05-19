16c1f782173303d16e986792dd647e8362317b8b
Azimbom Pino <pedronkwale@gmail.com>
2024-05-19 03:41:55 +0100

    my first web app

---
+++ demo/demo/src/main/java/com/dema/mavesports/demo/MavinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dema.mavesports.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavinDemoApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/dema/mavesports/demo/MavinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dema.mavesports.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
